5630a4e4176f9bb9bc9ebd0066185991813d640f
manths <sagarmanthan0001@gmail.com>
2020-06-12 18:02:48 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
+class Demo extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Route path='/' component={App} />
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-    <App />,
+  <Demo />,
   document.getElementById('root')
 );
